194875f3480c64d8feeae315cad11183c421b848
Viktor Savitskiy <savitvi8@gmail.com>
2016-05-02 10:56:44 -0500

    Added files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+	<?php
+	echo '
+		<footer>
+		<div class="row">
+		<div class="col-sm-12 col-md-12">
+			<p class="text-center">Website Created by Viktor V. Savitskiy</p>
+		</div>
+		</div>
+		</footer>
+			</div>
+	<!-- scripts-->
+	<script src="scripts/jquery-2.1.4.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="bootstrap-3.3.4-dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>'; ?>
